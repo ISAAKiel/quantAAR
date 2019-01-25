ecaed29d1017b99e0445bea449e717f2fe7352f8
Clemens Schmid <clemens@nevrome.de>
2019-01-25 14:52:50 +0100

    further experiments with the tidyseriation interface

--- R/seriation_utility_functions.R
+++ R/seriation_utility_functions.R
@@ -8,20 +8,19 @@
 tidyseriation <- function(
   x,
   dist_args = list(method = "euclidian"),
-  seriate_args = list(method = "Spectral")
+  seriate_args = list(method = "PCA")
 ) {
 
-  if (is.matrix(x) | is.dist(x)) {
-    seriate_args$x <- x
-  } else {
-    dist_args$x <- as.matrix(x)
-    seriate_args$x <- do.call(stats::dist, dist_args)
-  }
+  x_matrix <- as.matrix(x)
+  # dist_args$x <- x_matrix
+  # seriate_args$x <- do.call(stats::dist, dist_args)
+  seriate_args$x <- x_matrix
 
   seriation_result <- do.call(seriation::seriate, seriate_args)
-  seriation_order <- seriation::get_order(seriation_result)
+  seriation_order_rows <- seriation::get_order(seriation_result, dim = 1)
+  seriation_order_cols <- seriation::get_order(seriation_result, dim = 2)
 
-  x_matrix_reordered <- x_matrix[seriation_order,]
+  x_matrix_reordered <- x_matrix[seriation_order_rows, seriation_order_cols]
   x_tibble_reordered <- tibble::as_tibble(x_matrix_reordered)
 
   return(x_tibble_reordered)
