f37b97b0898f1277a27a78029f47257648e29e55
Clemens Schmid <clemens@nevrome.de>
2019-01-25 14:32:24 +0100

    work on tidyseriation interface

--- R/seriation_utility_functions.R
+++ R/seriation_utility_functions.R
@@ -1,16 +1,26 @@
 #' tidyseriation
 #'
 #' @param x test
-#' @param dist_method test
-#' @param seriate_method test
+#' @param dist_args test
+#' @param seriate_args test
 #'
 #' @export
-tidyseriation <- function(x, dist_method = "euclidian", seriate_method = "Spectral") {
+tidyseriation <- function(
+  x,
+  dist_args = list(method = "euclidian"),
+  seriate_args = list(method = "Spectral")
+) {
 
-  x_matrix <- as.matrix(x)
-  x_dist <- stats::dist(x_matrix, dist_method)
-  seriation_result <- seriation::seriate(x_dist, seriate_method)
+  if (is.matrix(x) | is.dist(x)) {
+    seriate_args$x <- x
+  } else {
+    dist_args$x <- as.matrix(x)
+    seriate_args$x <- do.call(stats::dist, dist_args)
+  }
+
+  seriation_result <- do.call(seriation::seriate, seriate_args)
   seriation_order <- seriation::get_order(seriation_result)
+
   x_matrix_reordered <- x_matrix[seriation_order,]
   x_tibble_reordered <- tibble::as_tibble(x_matrix_reordered)
 
